f25c31962da5f7442c7f6a4aedd82e6bf2b4255d
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-25 18:06:24 -0300

    improving Square

--- square.py
+++ square.py
@@ -3,26 +3,21 @@ import pygame
 
 pygame.init()
 
-COLORS = [(200,200,0),(200,0,200),(0,200,200)]
+COLORS = [(200,200,0),(200,0,200),(0,200,200),(100,100,0),(100,0,100),(0,100,100)]
 
 class Square:
     def __init__(self,chromosome,window_size):
         self.window_size = window_size
-        self.init_pos = (random.randint(0,self.window_size[0]), random.randint(0,self.window_size[1]))
+        self.init_pos = (random.randint(1,self.window_size[0]), random.randint(1,self.window_size[1]))
         self.rect = pygame.Rect(self.init_pos[0],self.init_pos[1],chromosome[0],chromosome[0])
         self.vel = chromosome[1]
-        self.vel_x = 0
-        self.vel_y = 0
+        self.vel_x = self.vel
+        self.vel_y = self.vel
 
         if random.random() < 0.5:
-            self.vel_x = self.vel
-        else:
-            self.vel_x =-self.vel_x
-
+            self.vel_x = - self.vel_x
         if random.random() < 0.5:
-            self.vel_y = self.vel
-        else:
-            self.vel_y = -self.vel
+            self.vel_y = - self.vel_y
 
         self.color = COLORS[random.randrange(0,len(COLORS))]
 
@@ -36,8 +31,8 @@ class Square:
         self.rect.x += self.vel_x
         self.rect.y += self.vel_y
 
-        if self.rect.x <= 0 or self.rect.x >= self.window_size[0]:
-            self.vel_x *= -1
-        
-        if self.rect.y <= 0 or self.rect.y >= self.window_size[1]:
-            self.vel_y *= -1
+        if self.rect.x < 0 or self.rect.x > self.window_size[0] - self.rect.width:
+            self.vel_x = - self.vel_x
+            
+        if self.rect.y < 0 or self.rect.y > self.window_size[1] - self.rect.height:
+            self.vel_y = - self.vel_y
