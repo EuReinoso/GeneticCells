8b652d02b7baa7cde2db3d1ab81f41f761d6b179
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-25 17:21:47 -0300

    population initial

--- genetic.py
+++ genetic.py
@@ -1,7 +1,11 @@
+from individual import Individual
+
 class Genetic:
     def __init__(self,population_size):
         self.population_size = population_size
         self.population = []
         
 
-        
+    def population_init(self,window_size):
+        for i in range(self.population_size):
+            population.append(Individual(window_size))
