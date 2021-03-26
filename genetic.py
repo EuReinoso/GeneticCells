425857df19f75b6cc30776175275deafa65a68c7
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-26 08:24:41 -0300

    best_individual

--- genetic.py
+++ genetic.py
@@ -4,8 +4,14 @@ class Genetic:
     def __init__(self,population_size):
         self.population_size = population_size
         self.population = []
+        self.best_solution = 0 
         
 
     def population_init(self,window_size):
         for i in range(self.population_size):
-            population.append(Individual(window_size))
+            self.population.append(Individual(window_size))
+        self.best_solution = self.population[0] #temp
+
+    def best_individual(self,individual):
+        if individual.grade > self.best_solution.grade:
+            self.best_solution = individual
