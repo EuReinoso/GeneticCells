eaa34404ee810f6ec3ee727018d5c169c936f0b3
EuReinoso <lucas.reinoso.br@gmail.com>
2021-03-26 08:00:39 -0300

    food_list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame,sys
 from square import Square
 from individual import Individual
 from genetic import Genetic
+from food import Food
 
 pygame.init()
 
@@ -11,7 +12,9 @@ RED = (255,0,0)
 
 WINDOW_SIZE = (640,480)
 
-POPULATION_SIZE = 150
+POPULATION_SIZE = 50
+
+FOOD_QUANT = 50
 
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("GeneticCells")
@@ -22,10 +25,21 @@ genetic.population_init(WINDOW_SIZE)
 time = pygame.time.Clock()
 fps = 60
 
+food_list = []
+def food_init():
+    for i in range(FOOD_QUANT):
+        food_list.append(Food(WINDOW_SIZE))
+
+def draw_food():
+    for food in food_list:
+        food.draw(window)
+
 def draw_population():
     for individual in genetic.population:
         individual.square.draw(window)
         individual.square.update()
+
+food_init()
 
 while True:
     window.fill(BLACK)
@@ -34,7 +48,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    
+    draw_food()
     draw_population()
 
     pygame.display.update()
